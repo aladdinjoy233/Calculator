86df1e7b9d580eaa496148589d166c04291a8cb6
aladdinjoy233 <ninjalover249@gmail.com>
2021-11-05 08:34:46 -0300

    Finish HTML and CSS styling of the calculator.

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,7 @@
+// Basic Math Functions
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+// Operate Function
+const operate = (operator, a, b) => operator(a, b);
